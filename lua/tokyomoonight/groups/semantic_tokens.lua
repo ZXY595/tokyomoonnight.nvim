7200fd08f32e04fa9f2debb7404a841679bc30df
ZXY <3303057136@qq.com>
2025-06-03 01:33:06 +0800

    Change semantic tokens

--- lua/tokyomoonight/groups/semantic_tokens.lua
+++ lua/tokyomoonight/groups/semantic_tokens.lua
@@ -14,10 +14,11 @@ function M.get(c)
     ["@lsp.type.enum"]                         = "@type",
     ["@lsp.type.enum.defaultLibrary"]          = "@type",
     ["@lsp.type.enumMember"]                   = "@constant",
+    ["@lsp.type.enumMember.defaultLibrary"]    = "@constant.builtin",
     ["@lsp.type.escapeSequence"]               = "@string.escape",
     ["@lsp.type.formatSpecifier"]              = "@markup.list",
     ["@lsp.type.generic"]                      = "@variable",
-    ["@lsp.type.interface"]                    = { fg = c.purple2 },
+    ["@lsp.type.interface"]                    = { fg = c.blue6, underdouble = true, bold = true },
     ["@lsp.type.keyword"]                      = "@keyword",
     ["@lsp.type.lifetime"]                     = "@keyword.storage",
     ["@lsp.type.namespace"]                    = "@module",
@@ -32,39 +33,34 @@ function M.get(c)
     ["@lsp.type.typeAlias"]                    = "@type.definition",
     ["@lsp.type.unresolvedReference"]          = { undercurl = true, sp = c.error },
     ["@lsp.type.variable"]                     = {}, -- use treesitter styles for regular variables
-    ["@lsp.typemod.class.defaultLibrary"]      = "@type.builtin",
-    ["@lsp.typemod.enum.defaultLibrary"]       = "@type.builtin",
-    ["@lsp.typemod.enumMember.defaultLibrary"] = "@constant.builtin",
+    ["@lsp.typemod.enum"]                      = { underdashed = true },
+    ["@lsp.typemod.enumMember"]                = { underdashed = true },
+    ["@lsp.typemod.enum.defaultlibrary"]       = { underdashed = true },
+    ["@lsp.typemod.enumMember.defaultlibrary"] = { underdashed = true },
     ["@lsp.typemod.function.defaultLibrary"]   = "@function.builtin",
     ["@lsp.typemod.keyword.async"]             = "@keyword.coroutine",
     ["@lsp.typemod.keyword.injected"]          = "@keyword",
+    ["@lsp.typemod.keyword.unsafe"]            = { fg = c.error }, -- unsafe
     ["@lsp.typemod.macro.defaultLibrary"]      = "@function.builtin",
     ["@lsp.typemod.method.defaultLibrary"]     = "@function.builtin",
     ["@lsp.typemod.operator.injected"]         = "@operator",
     ["@lsp.typemod.string.injected"]           = "@string",
-    ["@lsp.typemod.struct.defaultLibrary"]     = "@type.builtin",
+    ["@lsp.typemod.struct.defaultLibrary"]     = "@type",
+    ["@lsp.typemod.class.defaultLibrary"]      = "@type",
     -- ["@lsp.typemod.type.defaultLibrary"]       = { fg = Util.blend_bg(c.blue1, 0.8) },
     -- ["@lsp.typemod.typeAlias.defaultLibrary"]  = { fg = Util.blend_bg(c.blue1, 0.8) },
     ["@lsp.typemod.variable.callable"]         = "@function",
     ["@lsp.typemod.variable.defaultLibrary"]   = "@variable.builtin",
     ["@lsp.typemod.variable.injected"]         = "@variable",
     ["@lsp.typemod.variable.static"]           = "@constant",
+    ["@lsp.typemod.variable.constant"]         = "@constant",
+    ["@lsp.typemod.variable.mutable"]          = { fg = c.blue3, underline = true },
+    ["@lsp.typemod.method.mutable"]            = { fg = c.blue3, underline = true },
+    ["@lsp.mod.deprecated"]                    = { strikethrough = true },
 
-
-    -- -- Rust Groups
     -- ["@type.qualifier.rust"] = { underline = true },
-    ["@lsp.typemod.keyword.unsafe.rust"] = { fg = c.error }, -- rust unsafe
     -- ["@lsp.typemod.typealias.declaration.rust"] = { underline = true }, -- type alias lvalue
-    ["@lsp.typemod.enummember.defaultlibrary.rust"] = { underdashed = true },
-    ["@lsp.typemod.enum.defaultlibrary.rust"] = { underdashed = true },
-    -- ["@lsp.type.variable.rust"] = { fg = c.white },
-    ["@lsp.typemod.variable.constant.rust"] = "@constant",
-    -- ["@lsp.type.property.rust"] = { underline = true },
-    -- ["@lsp.type.parameter.rust"] = { underline = true },
-    ["@lsp.typemod.variable.mutable.rust"] = { fg = c.blue3, underline = true },
-    ["@lsp.typemod.method.mutable.rust"] = { fg = c.blue3, underline = true },
     -- ["@lsp.typemod.parameter.declaration.rust"] = { fg = c.pink },
-    ["@lsp.mod.deprecated.rust"] = { strikethrough = true },
     -- ["@lsp.mod.async"] = { italic = true }
   }
 end
